fe59e1a034abc5f2459d510056d31d4d84f80d99
anotherjsguy <kuldeepkeshwar@gmail.com>
2020-07-04 17:57:51 +0530

    add alignments

--- src/StackWithGrid.js
+++ src/StackWithGrid.js
@@ -1,25 +1,46 @@
+import PropTypes from "prop-types";
 import React from "react";
 import styled from "@emotion/styled";
+const alignments = [
+  "start",
+  "end",
+  "center",
+  "stretch",
+  "gap-around",
+  "gap-between",
+  "gap-evenly"
+];
+const alignmentsY = ["start", "end", "center", "stretch"];
 
 function gridStyles({ direction = "vertical", gap, children }) {
   if (direction === "vertical") {
     return `
-    grid-row-gap: ${gap || 0};
-    grid-template-columns:auto;
-    grid-template-rows: repeat(${React.Children.count(children)}, min-content);
-  `;
+      grid-row-gap: ${gap || 0};
+      grid-template-columns:auto;
+      grid-template-rows: repeat(${React.Children.count(
+        children
+      )}, min-content);
+    `;
   } else {
     return `
-    grid-column-gap: ${gap || 0};
-    grid-template-rows:auto;
-    grid-template-columns: repeat(${React.Children.count(
-      children
-    )}, min-content);
-    `;
+      grid-column-gap: ${gap || 0};
+      grid-template-rows:auto;
+      grid-template-columns: repeat(${React.Children.count(
+        children
+      )}, min-content);
+      `;
   }
 }
 
 export const Stack = styled.div`
   display: grid;
-  ${gridStyles}
+  justify-content: ${({ align }) => align || "stretch"};
+  align-items: ${({ alignY }) => alignY || "stretch"};
+  ${gridStyles};
 `;
+Stack.propTypes = {
+  gap: PropTypes.string,
+  align: PropTypes.oneOf(alignments),
+  alignY: PropTypes.oneOf(alignmentsY),
+  direction: PropTypes.oneOf(["horizontal", "vertical"])
+};
